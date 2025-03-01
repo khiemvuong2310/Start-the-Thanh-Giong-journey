25c89c111d3209c1d8d538f8d95381f6005f13d5
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2025-03-01 09:49:19 +0700

    Create Dash , enemy animation death

--- Assets/Scipts/Enemies/EnemyHealth.cs
+++ Assets/Scipts/Enemies/EnemyHealth.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int startingHealth = 3;
+    [SerializeField] private GameObject deathVFXPrefab;
+    [SerializeField] private float knockBackThrust = 15f;
 
     private int currentHealth;
     private Knockback knockback;
+    private Flash flash;
 
     private void Awake()
     {
+        flash = GetComponent<Flash>();
         knockback = GetComponent<Knockback>();
     }
 
@@ -22,7 +26,14 @@ public class EnemyHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        knockback.GetKnockedBack(PlayerController.Instance.transform, 15f);
+        knockback.GetKnockedBack(PlayerController.Instance.transform, knockBackThrust);
+        StartCoroutine(flash.FlashRoutine());
+        StartCoroutine(CheckDetectDeathRoutine());
+    }
+
+    private IEnumerator CheckDetectDeathRoutine()
+    {
+        yield return new WaitForSeconds(flash.GetRestoreMatTime());
         DetectDeath();
     }
 
@@ -30,6 +41,7 @@ public class EnemyHealth : MonoBehaviour
     {
         if (currentHealth <= 0)
         {
+            Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
